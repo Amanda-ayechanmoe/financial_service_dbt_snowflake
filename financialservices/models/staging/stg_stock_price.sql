a841872c76cda737a318f3d021034787ae47a00b
Amanda Moe <amandaayechanmoe@gmail.com>
2025-03-02 17:40:39 +0800

    add stg stock price and edit gitignore

---
+++ financialservices/models/staging/stg_stock_price.sql
@@ -0,0 +1,17 @@
+with source as (
+    select * from {{source('finance','raw_stock_price')}}
+),
+
+renamed as (
+    select 
+        "TICKER" as ticker,
+        "ASSET_CLASS" as asset_class,
+        "PRIMARY_EXCHANGE_CODE" as exchange_code,
+        "PRIMARY_EXCHANGE_NAME" as exchange_name,
+        "VARIABLE" as indicator_name,
+        "DATE" as stock_date,
+        "VALUE" as stock_value
+    from source
+)
+
+select * from renamed
